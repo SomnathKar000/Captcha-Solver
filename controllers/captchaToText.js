08773b29c912445a6686c6222a239b35f3871720
SomnathKar000 <somnathkar201901@gmail.com>
2023-04-04 22:20:33 +0530

    Commit

--- controllers/captchaToText.js
+++ controllers/captchaToText.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const axios = require("axios");
 const Tesseract = require("tesseract.js");
+const { createCanvas, loadImage } = require("canvas");
 
 const image = "captcha.jpg";
 
@@ -9,16 +10,28 @@ const TextConverter = async (req, res) => {
   axios({ method: "get", url: imageUrl, responseType: "stream" })
     .then((response) => {
       response.data.pipe(fs.createWriteStream(image)).on("close", () => {
-        Tesseract.recognize(image, "eng", {
-          logger: (m) => console.log(m),
-        })
-          .then(({ data: { text } }) => {
-            res.status(200).json({ text, imageUrl });
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(500).json({ err });
-          });
+        console.log("Image downloaded successfully");
+        // Step 2: Load the image using canvas and extract the captcha image
+        loadImage(image).then((image) => {
+          const canvas = createCanvas(image.width, image.height);
+          const context = canvas.getContext("2d");
+
+          // Draw the image onto the canvas context
+
+          context.drawImage(image, 0, 0, image.width, image.height);
+          const captcha = canvas.toDataURL("image/png");
+
+          // Step 3: Use Tesseract.js to recognize the captcha text
+          Tesseract.recognize(captcha)
+            .then((result) => {
+              console.log("Solved captcha:");
+              res.status(200).json(result.data.text);
+            })
+            .catch((err) => {
+              console.log("Error solving captcha:", err);
+              res.status(500).json(err);
+            });
+        });
       });
     })
     .catch((err) => {
